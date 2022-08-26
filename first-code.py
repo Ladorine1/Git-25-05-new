f90a1049d9af633f4e571d82af9d8b2421ad3cbb
Ladorine1 <107524010+Ladorine1@users.noreply.github.com>
2022-08-26 01:28:02 -0400

    Second change

--- first-code.py
+++ first-code.py
@@ -2,5 +2,5 @@ import datetime
 now = datetime.datetime.now()
 print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
-print ("Hello guys !! learning git ")
-Print ("I am learning GIT on AWS " )
+print ("Hello guys !! learning GIT ")
+Print ("I am learning GIT on AWS. " )
